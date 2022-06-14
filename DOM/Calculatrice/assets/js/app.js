bb4457a8d18a04fa1ff280a36ea65b2386bad804
Arick Bulakali <arickbulakali@gmail.com>
2022-06-14 15:17:00 +0200

    feat: calculate interaction

--- DOM/Calculatrice/assets/js/app.js
+++ DOM/Calculatrice/assets/js/app.js
@@ -1,5 +1,23 @@
+const output = document.getElementById('output');
 const clear = document.getElementById('clear');
+const equal = document.getElementById('equal');
 
 clear.addEventListener('click', function () {
   document.querySelector('.value').value = "";
 });
+const nums = document.querySelectorAll('.num');
+nums.forEach(num=> {
+  num.addEventListener('click', function (evt) {
+    let textValue = evt.target.textContent;
+    output.value += textValue;
+  })
+})
+equal.addEventListener('click', ()=> {
+    if(output.value.length >1) {
+      output.value = eval(output.value)
+    }
+})
+
+
+
+document.querySelector('.calculator').addEventListener('submit',(evt)=>evt.preventDefault())
