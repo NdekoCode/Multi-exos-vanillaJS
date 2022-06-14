9fcfc948b16ec7dfa811b983d7443ff70e483201
Arick Bulakali <arickbulakali@gmail.com>
2022-06-14 09:03:40 +0200

    feat: basic JS

---
+++ DOM/MinMax/assets/js/script.js
@@ -0,0 +1,6 @@
+const aleatNumber = parseInt(Math.random() * 10 + 1);
+let userNumber = document.getElementById("userNumber");
+let btn = document.getElementById("btn-form");
+let output = document.getElementById("output");
+output.style.textAlign = "center";
+let tentative = 4;
